12c85297b39722c59bfe2c7a95b4838b16d1b052
julieweeds <julie.weeds@gmail.com>
2013-02-19 12:30:34 +0000

    still debugging nan

--- src/stsdata.py
+++ src/stsdata.py
@@ -217,7 +217,7 @@ class STSData:
     def testpoly(self,subset,excl,type):
 
         thispoly = self.fitpoly(subset,excl,type)
-        print thispoly
+        #print thispoly
 
         fileid =1
         predictions=[]
@@ -240,6 +240,7 @@ class STSData:
         x=numpy.array(predictions)
         y=numpy.array(gs)
         print len(x),len(y)
+        print x,y
         pr = stats.spearmanr(x,y)
         if excl==1 and self.show==True:
             mytitle="Correlation for: "+subset+": "+str(excl)+": "+type
